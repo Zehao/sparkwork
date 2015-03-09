65fc85765cc3bbdcd4f28482010cbfa8f10192f5
zehao <zehaojin@gmail.com>
2015-03-09 19:14:18 +0800

    add spark pi Signed-off-by: zehao <zehaojin@gmail.com>

---
+++ src/com/spark/MyPI/MyPI.scala
@@ -0,0 +1,25 @@
+package com.spark.MyPI
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+import scala.math._
+
+/**
+ * Created by Zehao on 2015/3/9.
+ */
+object MyPI {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("MyPI")
+    val spark = new SparkContext(conf)
+    val slices = if (args.length > 0) args(0).toInt else 2
+    val n = 100000 * slices
+    val count = spark.parallelize(1 to n, slices).map { i =>
+      val x = random * 2 - 1
+      val y = random * 2 - 1
+      if (x*x + y*y < 1) 1 else 0
+    }.reduce(_ + _)
+    println("Pi is roughly " + 4.0 * count / n)
+    spark.stop()
+  }
+}
